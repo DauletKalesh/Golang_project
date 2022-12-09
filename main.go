77fbca4917a63cfe835d07293f250ff884a275a3
altynbekM01 <altynbek.01m@gmail.com>
2022-12-09 20:46:09 +0600

    Altynbek_search

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"jwt-authentication-golang/controllers"
 	"jwt-authentication-golang/database"
 	"jwt-authentication-golang/middlewares"
+	"jwt-authentication-golang/models"
+	"math"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gofiber/fiber"
 )
 
 func main() {
@@ -24,7 +28,8 @@ func main() {
 	router := initRouter()
 	router.Run(":8080")
 }
-//asem push
+
+// asem push
 func initRouter() *gin.Engine {
 	router := gin.Default()
 	api := router.Group("/api")
@@ -42,6 +47,42 @@ func initRouter() *gin.Engine {
 
 		api.GET("/celebrity/get/:celebrityId", controllers.GetCelebrity)
 		api.GET("/celebrities", controllers.GetCelebrites)
+
+		api.GET("/movie/search", func(context *gin.Context) {
+			var movies []models.Movie
+
+			sql := "SELECT * FROM movies"
+
+			if s := context.Query("s"); s != "" {
+				sql = fmt.Sprintf("%s WHERE name LIKE '%%%s%%' OR description LIKE '%%%s%%'", sql, s, s)
+			}
+
+			if sort := context.Query("sort"); sort != "" {
+				sql = fmt.Sprintf("%s ORDER BY rating %s", sql, sort)
+			}
+
+			database.Instance.Raw(sql).Scan(&movies)
+			//context.JSON(200, &movies)
+
+			page, _ := strconv.Atoi(context.Query("page"))
+			perPage := 3
+			var total int64
+
+			database.Instance.Raw(sql).Count(&total)
+
+			sql = fmt.Sprintf("%s LIMIT %d OFFSET %d", sql, perPage, (page-1)*perPage)
+
+			database.Instance.Raw(sql).Scan(&movies)
+
+			context.JSON(200, fiber.Map{
+				"data":      &movies,
+				"total":     total,
+				"page":      page,
+				"last_page": math.Ceil(float64(total / int64(perPage))),
+			})
+
+			return
+		})
 
 		api.GET("/movie/get/:movieId", controllers.GetMovie)
 		// crud_movie := api.Group(("/movie")).Use()
